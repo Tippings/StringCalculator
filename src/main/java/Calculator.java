3a2b841b2319ce9e13d2b78c4e117bcbf12771aa
Tippings <susanna.tipping@gmail.com>
2020-12-13 20:56:42 +0100

    negativeNumber

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,14 +4,21 @@ public class Calculator {
     public Calculator(){
         seperator = new Seperator();
     }
-    public int add(String inputString) {
+    public int add(String inputString) throws Exception {
        if("".equals(inputString))
            return 0;
 
         String[] numbers = seperator.split(inputString);
+
         return addArray(numbers);
     }
-    private int addArray (String [] numbers){
+    private int addArray (String [] numbers) throws Exception {
+        for (String number : numbers) {
+            if (convertToInt(number)<0){
+                throw new Exception("Negative number not alowed!");
+            }
+        }
+
         int sum = 0;
         for (String number : numbers) {
             sum += convertToInt(number);
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,33 +7,38 @@ public class CalculatorTest {
 
     Calculator calculator = new Calculator();
     @Test
-    public void returnZero(){
+    public void returnZero() throws Exception {
         assertEquals(0, calculator.add (""));
     }
     @Test
-    public void returnNumber(){
+    public void returnNumber() throws Exception {
         assertEquals(1, calculator.add ("1"));
     }
     @Test
-    public void returnNumbers(){
+    public void returnNumbers() throws Exception {
         assertEquals(2, calculator.add ("2"));
     }
     @Test
-    public void numberCommaNumberSum (){
+    public void numberCommaNumberSum () throws Exception {
         assertEquals(3, calculator.add("1,2"));
 
     }
     @Test
-    public void manyNumberCommaSum (){
+    public void manyNumberCommaSum () throws Exception {
         assertEquals(15, calculator.add("1,2,5,4,3"));
     }
     @Test
-    public void handleNewLines (){
+    public void handleNewLines () throws Exception {
         assertEquals(6, calculator.add("1\n2,3"));
     }
     @Test
-    public void handleAnyDelimiter (){
+    public void handleAnyDelimiter () throws Exception {
         assertEquals(3, calculator.add("//;\n1;2"));
+    }
+    @Test
+    public void negativeNumber () throws Exception {
+        calculator.add("-1");
+
     }
 
 }
